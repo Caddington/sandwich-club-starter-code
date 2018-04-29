ebf479b367953d4d95b1b95935c86c52abda65e1
Christopher Addingtongit config --global user.name Christopher <caddington@fullsail.edu>
2018-04-29 10:11:46 -0700

    Partway through building out JsonUtils.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,35 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class JsonUtils {
 
+    private static final String TAG = JsonUtils.class.getSimpleName();
+
+    private static final String JSON_NAME_ARRAY_KEY = "name";
+    private static final String JSON_MAIN_NAME_KEY = "mainName";
+    private static final String JSON_AKA_KEY = "alsoKnownAs";
+    private static final String JSON_ORIGIN_KEY = "placeOfOrigin";
+    private static final String JSON_DESCRIPTION_KEY = "description";
+    private static final String JSON_IMAGE_KEY = "image";
+    private static final String JSON_INGREDIENTS_KEY = "ingredients";
+
+
     public static Sandwich parseSandwichJson(String json) {
+
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+
+            Sandwich sandwich = new Sandwich(sandwichJson.getJSONArray())
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing JSON: " + e.getMessage());
+        }
+
         return null;
     }
 }
